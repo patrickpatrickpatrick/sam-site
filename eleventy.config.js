80ebf221cb9a1f78d09f57ac3ba88db01f4ade65
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-09-17 16:09:39 +0100

    add fetch

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,7 +10,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
 
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
